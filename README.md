6063a46588f03a0b7a558b477f1a4cc43b763813
Dmitry Slinkov <dns@matterlabs.dev>
2023-10-13 20:38:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ethkl2023-monitoring-contracts
+
+This repository contains a sample implementation of OpenZeppelin Defender 2.0 configuration and its deployment pipeline that can be used as the starting point to build the on-chain monitoring solution for your project.
+
+## Pipeline configuration
+Pipeline uses defender credentials stored in variables:
+
+DEFENDER_API_KEY and DEFENDER_API_SECRET
+
+You can get it when you register a free account in OpenZeppelin Defender.
+
---
+++ contract/sample_erc20.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract ETKHLDemoToken is ERC20, ERC20Burnable, ERC20Pausable, AccessControl {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(address defaultAdmin, address pauser, address minter)
+        ERC20("ETHKL23 Monitoring Demo", "MDT")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(PAUSER_ROLE, pauser);
+        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
+    }
+}
